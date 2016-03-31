a28205026ba2ddd185178f87aa7e530193ea418c
Mattia Natali <matitalatina@gmail.com>
2016-03-31 11:49:15 +0200

    Async fetch, finalised android

--- src/android/CookieBroker.java
+++ src/android/CookieBroker.java
@@ -1,5 +1,7 @@
 package ch.wavein.plugin.cookie;
 
+import android.util.Log;
+
 import org.apache.cordova.*;
 import org.chromium.mojom.mojo.HttpResponse;
 import org.json.JSONArray;
@@ -12,59 +14,84 @@ import java.net.CookieManager;
 import java.net.CookiePolicy;
 import java.net.HttpCookie;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class CookieBroker extends CordovaPlugin {
     static final String COOKIES_HEADER = "Set-Cookie";
+    static final String CLASS_TAG = "CookieBroker";
 
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("getCookies")) {
+            final String url = data.getString(0);
+            final JSONObject opts = data.optJSONObject(1);
 
-            String url = data.getString(0);
-
-            HttpURLConnection ucon = null;
-            try {
-                ucon = (HttpURLConnection) new URL(url).openConnection();
-                ucon.setInstanceFollowRedirects(false);
-                ucon.setRequestProperty("User-Agent", "Mozilla/5.0 (Linux; U; Android-phone)");
-
-                Map<String, List<String>> headerFields = ucon.getHeaderFields();
-                List<String> cookiesHeader = headerFields.get(COOKIES_HEADER);
-                List<HttpCookie> allCookies = new ArrayList<HttpCookie>();
-
-                for (String rawCookie : cookiesHeader) {
-                    allCookies.addAll(HttpCookie.parse(rawCookie));
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    try {
+                        JSONArray cookies = getCookies(url, opts);
+                        callbackContext.success(cookies);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        callbackContext.error("Can't reach the server");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        callbackContext.error("Can't parse the JSON");
+                    }
                 }
+            });
 
-                JSONArray cookiesJson = new JSONArray();
-
-                for (HttpCookie cookie:allCookies) {
-                    JSONObject cookieJson = new JSONObject();
-                    cookieJson.put("name", cookie.getName());
-                    cookieJson.put("value", cookie.getValue());
-                    cookieJson.put("domain", cookie.getDomain());
-                    cookieJson.put("discard", cookie.getDiscard());
-                    cookieJson.put("maxAge", cookie.getMaxAge());
-                    cookieJson.put("path", cookie.getPath());
-                    cookiesJson.put(cookieJson);
-                }
-                callbackContext.success(cookiesJson);
-            } catch (IOException e) {
-                e.printStackTrace();
-                callbackContext.error("Can't reach the server");
-            }
-            
             return true;
 
         } else {
-            
+
             return false;
 
         }
+    }
+
+    private JSONArray getCookies(String url, JSONObject opts) throws IOException, JSONException {
+        if (opts == null) {
+            opts = new JSONObject();
+        }
+        String cookieString = opts.optString("cookieHeader", "");
+        //Log.d(CLASS_TAG, "cookieString -> " + cookieString);
+
+        HttpURLConnection ucon = (HttpURLConnection) new URL(url).openConnection();
+        ucon.setInstanceFollowRedirects(false);
+        ucon.setRequestProperty("User-Agent", "Mozilla/5.0 (Linux; U; Android-phone)");
+
+        if (!cookieString.equals("")) {
+            ucon.setRequestProperty("Cookie", cookieString);
+        }
+
+        Map<String, List<String>> headerFields = ucon.getHeaderFields();
+        List<String> cookiesHeader = headerFields.get(COOKIES_HEADER);
+        List<HttpCookie> allCookies = new ArrayList<HttpCookie>();
+
+        for (String rawCookie : cookiesHeader) {
+            allCookies.addAll(HttpCookie.parse(rawCookie));
+        }
+
+        JSONArray cookiesJson = new JSONArray();
+
+        for (HttpCookie cookie : allCookies) {
+            JSONObject cookieJson = new JSONObject();
+            cookieJson.put("name", cookie.getName());
+            cookieJson.put("value", cookie.getValue());
+            cookieJson.put("domain", cookie.getDomain());
+            cookieJson.put("discard", cookie.getDiscard());
+            cookieJson.put("maxAge", cookie.getMaxAge());
+            cookieJson.put("path", cookie.getPath());
+            cookiesJson.put(cookieJson);
+        }
+
+        return cookiesJson;
     }
 }
--- www/cookie-broker.js
+++ www/cookie-broker.js
@@ -1,25 +1,27 @@
-(function () {
-  'use strict';
-  /*global cordova, module*/
-  var exec = cordova.require('cordova/exec');
-  var cookieBroker = {};
-  cookieBroker.getCookies = getCookies;
+cordova.define("cordova-cookie-broker.CookieBroker", function (require, exports, module) {
+  (function () {
+    'use strict';
+    /*global cordova, module*/
+    var exec = cordova.require('cordova/exec');
+    var cookieBroker = {};
+    cookieBroker.getCookies = getCookies;
 
-  function getCookies(url) {
-    return new Promise(function (resolve, reject) {
-      cordova.exec(successCallback, errorCallback, "CookieBroker", "getCookies", [url]);
+    function getCookies(url, opts) {
+      return new Promise(function (resolve, reject) {
+        cordova.exec(successCallback, errorCallback, "CookieBroker", "getCookies", [url, opts]);
 
-      function successCallback(cookieList) {
-        resolve(cookieList);
-      }
+        function successCallback(cookieList) {
+          resolve(cookieList);
+        }
 
-      function errorCallback(response) {
-        reject(response);
-      }
-    });
+        function errorCallback(response) {
+          reject(response);
+        }
+      });
 
 
-  };
+    };
 
-  module.exports = cookieBroker;
-})();
+    module.exports = cookieBroker;
+  })();
+});
